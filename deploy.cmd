bb9bc1f734fbae53761d8108c27f98140d9de012
William Wong <compulim@hotmail.com>
2016-08-31 03:49:00 +0800

    Cleanup dist on build

--- deploy.cmd
+++ deploy.cmd
@@ -69,6 +69,9 @@ IF DEFINED CLEAN_LOCAL_DEPLOYMENT_TEMP (
   mkdir "%DEPLOYMENT_INTERMEDIATE%"
 )
 
+IF NOT DEFINED NODE_EXE SET NODE_EXE=node
+IF NOT DEFINED NPM_CMD SET NPM_CMD=npm
+
 goto Deployment
 
 :: Utility Functions
@@ -128,11 +131,13 @@ IF !ERRORLEVEL! NEQ 0 goto error
 :: BUG: Currently, we must use Node.js < 6.0.0 to run Webpack, otherwise, memory-fs will fail
 pushd "%DEPLOYMENT_INTERMEDIATE%"
 
-:: call :ExecuteCmd !NPM_CMD! install --quiet
-call :ExecuteCmd npm install --quiet
+call :ExecuteCmd !NPM_CMD! run clean
 IF !ERRORLEVEL! NEQ 0 goto error
 
-call :ExecuteCmd npm dedupe
+call :ExecuteCmd !NPM_CMD! install --quiet
+IF !ERRORLEVEL! NEQ 0 goto error
+
+call :ExecuteCmd !NPM_CMD! dedupe
 IF !ERRORLEVEL! NEQ 0 goto error
 
 popd
