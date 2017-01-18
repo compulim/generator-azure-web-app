893d76dbf325561aeec7639cb017238a279a4c23
William Wong <compulim@hotmail.com>
2017-01-18 15:08:52 +0800

    Update gulp scripts

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,6 @@ const buffer     = require('vinyl-buffer');
 const config     = require('../config');
 const del        = require('del');
 // const filter     = require('gulp-filter');
-const gutil      = require('gulp-util');
 // const htmlmin    = require('gulp-htmlmin');
 // const imagemin   = require('gulp-imagemin');
 const install    = require('gulp-install');
@@ -16,36 +15,41 @@ const uglify     = require('gulp-uglify');
 const webpack    = require('webpack-stream');
 const Webpack    = require('webpack');
 
-const { basename, join, relative }          = require('path');
-const { globIgnoreNodeModules, prettyPath } = require('./util');
+const { magenta, red }                           = require('colors');
+const { basename, join, relative }               = require('path');
+const { globIgnoreNodeModules, log, prettyPath } = require('./util');
 
 module.exports = function (gulp) {
   gulp.task('build', [
-    'build:content',
-    'build:server',
-    'build:bundle'
+    'build:asset',
+    'build:bundle',
+    'build:config',
+    'build:server'
   ], build);
 
+  gulp.task('build:asset', buildAsset);
+  gulp.task('build:config', buildConfig);
   gulp.task('build:bundle', buildBundle);
-  gulp.task('build:content', buildContent);
   gulp.task('build:server', buildServer);
 
   gulp.task('rebuild', [
-    'rebuild:content',
-    'rebuild:server',
-    'rebuild:bundle'
+    'rebuild:asset',
+    'rebuild:bundle',
+    'rebuild:config',
+    'rebuild:server'
   ], build);
 
-  gulp.task('rebuild:bundle', ['clean:webroot'], buildBundle);
-  gulp.task('rebuild:content', ['clean:webroot'], buildContent);
-  gulp.task('rebuild:server', ['clean:webroot'], buildServer);
+  gulp.task('rebuild:asset', ['clean:website'], buildAsset);
+  gulp.task('rebuild:bundle', ['clean:website'], buildBundle);
+  gulp.task('rebuild:config', ['clean:website'], buildConfig);
+  gulp.task('rebuild:server', ['clean:website'], buildServer);
 
   function build() {
-    gutil.log('[build]', `Build with "${ process.env.NODE_ENV }" favor outputted to ${ prettyPath(config.DEST_WEBSITE_DIR) }`);
+    log('build', `Build with ${ magenta(process.env.NODE_ENV) } favor outputted to ${ prettyPath(config.DEST_WEBSITE_DIR) }`);
   }
 
-  function buildContent() {
-    gutil.log('[build:content]', `Copying content from ${ prettyPath(config.SOURCE_STATIC_FILES_DIR) } to ${ prettyPath(config.DEST_WEBSITE_STATIC_FILES_DIR) }`);
+  function buildAsset() {
+    log('build:asset', `Copying content from ${ prettyPath(config.SOURCE_STATIC_FILES_DIR) } to ${ prettyPath(config.DEST_WEBSITE_STATIC_FILES_DIR) }`);
 
     // const htmlFilter = filter(['*.htm', '*.html'], { restore: true });
     // const imageFilter = filter(['*.gif', '*.jpg', '*.png'], { restore: true });
@@ -61,15 +65,16 @@ module.exports = function (gulp) {
       .pipe(gulp.dest(config.DEST_WEBSITE_STATIC_FILES_DIR));
   }
 
-  function buildServer() {
-    gutil.log('[build:server]', `Copying code from ${ prettyPath(config.SOURCE_SERVER_DIR) } to ${ prettyPath(config.DEST_WEBSITE_DIR) }`);
+  function buildConfig() {
+    log('build:config', `Copying config from ${ prettyPath(config.SOURCE_DIR) } to ${ prettyPath(config.DEST_WEBSITE_DIR) }`);
 
     return gulp
       .src(
-        globIgnoreNodeModules(config.SOURCE_SERVER_DIR).concat(
-          join(__dirname, 'iisnode.yml'),
-          join(__dirname, '../package.json')
-        )
+        [
+          'iisnode.yml',
+          'package.json',
+          'web.config'
+        ].map(filename => join(config.SOURCE_DIR, filename))
       )
       .pipe(gulp.dest(config.DEST_WEBSITE_DIR))
       .pipe(install({
@@ -78,29 +83,41 @@ module.exports = function (gulp) {
       }));
   }
 
+  function buildServer() {
+    log('build:server', `Copying code from ${ prettyPath(config.SOURCE_SERVER_DIR) } to ${ prettyPath(config.DEST_WEBSITE_SERVER_DIR) }`);
+
+    return gulp
+      .src(
+        globIgnoreNodeModules(config.SOURCE_SERVER_DIR).concat(
+          `!${ join(config.SOURCE_SERVER_DIR) }/web.config`
+        )
+      )
+      .pipe(gulp.dest(config.DEST_WEBSITE_SERVER_DIR));
+  }
+
   function buildBundle() {
     if (process.env.BUNDLER === 'webpack') {
-      gutil.log('[build:bundle]', 'Bundling with Webpack');
+      log('build:bundle', `Bundling with ${ magenta('Webpack') }`);
 
       return buildWebpack();
     } else {
-      gutil.log('[build:bundle]', 'Bundling with rollup.js');
+      log('build:bundle', `Bundling with ${ magenta('rollup.js') }`);
 
       return buildRollup();
     }
   }
 
   function buildWebpack() {
-    gutil.log('[build:webpack]', `Using configuration from ${ prettyPath(config.SOURCE_WEBPACK_CONFIG_FILE) }`);
+    log('build:webpack', `Using configuration from ${ prettyPath(config.SOURCE_WEBPACK_CONFIG_FILE) }`);
 
     const WEBPACK_CONFIG = require(config.SOURCE_WEBPACK_CONFIG_FILE);
 
-    gutil.log('[build:webpack]', 'Bundling with entrypoints:', WEBPACK_CONFIG.entry.map(entry => prettyPath(entry)).join(', '));
-    gutil.log('[build:webpack]', `Will output to ${ prettyPath(config.DEST_WEBSITE_BUNDLE_FILE) }`);
+    log('build:webpack', 'Bundling with entrypoints:', WEBPACK_CONFIG.entry.map(entry => prettyPath(entry)).join(', '));
+    log('build:webpack', `Will output to ${ prettyPath(config.DEST_WEBSITE_BUNDLE_FILE) }`);
 
     const sourceMap = process.env.SOURCE_MAP === 'true';
 
-    sourceMap && gutil.log('[build:webpack]', 'Source map is enabled, this build should not be used for production');
+    log('build:webpack', red('Source map is enabled, this build should not be used for production'));
 
     const plugins = WEBPACK_CONFIG.plugins || (WEBPACK_CONFIG.plugins = []);
 
@@ -129,18 +146,18 @@ module.exports = function (gulp) {
   }
 
   function buildRollup() {
-    gutil.log('[build:rollup]', `Using configuration from ${ prettyPath(config.SOURCE_ROLLUP_CONFIG_FILE) }`);
+    log('build:rollup', `Using configuration from ${ prettyPath(config.SOURCE_ROLLUP_CONFIG_FILE) }`);
 
     const ROLLUP_CONFIG = require(config.SOURCE_ROLLUP_CONFIG_FILE)['default'];
 
-    gutil.log('[build:rollup]', 'Bundling with entrypoint:', ROLLUP_CONFIG.entry);
-    gutil.log('[build:rollup]', `Will output to ${ prettyPath(config.DEST_WEBSITE_BUNDLE_FILE) }`);
+    log('build:rollup', `Bundling with entrypoint ${ prettyPath(ROLLUP_CONFIG.entry) }`);
+    log('build:rollup', `Will output to ${ prettyPath(config.DEST_WEBSITE_BUNDLE_FILE) }`);
 
     const sourceMap = process.env.SOURCE_MAP === 'true';
 
     let workflow = rollup(Object.assign({}, ROLLUP_CONFIG, { rollup: require('rollup'), sourceMap })).pipe(source(basename(config.DEST_WEBSITE_BUNDLE_FILE))).pipe(buffer());
 
-    sourceMap && gutil.log('[build:rollup]', 'Source map is enabled, this build should not be used for production');
+    log('build:rollup', red('Source map is enabled, this build should not be used for production'));
 
     if (sourceMap) {
       workflow = workflow
