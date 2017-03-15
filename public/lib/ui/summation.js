f01bfd6c319bb8c03255e17803a8a27c29da1711
William Wong <compulim@hotmail.com>
2017-03-15 09:53:28 +0800

    Remove getter/setter for better IE8

--- public/lib/ui/summation.js
+++ public/lib/ui/summation.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// import React, { Component, PropTypes } from 'react';
-
 const { PropTypes } = React;
 
 export default class Summation extends React.Component {
@@ -10,24 +8,6 @@ export default class Summation extends React.Component {
 
     this.handleValueXChange = this.handleValueXChange.bind(this);
     this.handleValueYChange = this.handleValueYChange.bind(this);
-  }
-
-  static get propTypes() {
-    return {
-      onXChange: PropTypes.func,
-      onYChange: PropTypes.func,
-      x: PropTypes.number,
-      y: PropTypes.number
-    };
-  }
-
-  static get defaultProps() {
-    return {
-      onXChange: () => 0,
-      onYChange: () => 0,
-      x: 0,
-      y: 0
-    };
   }
 
   handleValueXChange(evt) {
@@ -42,11 +22,25 @@ export default class Summation extends React.Component {
     return (
       <div>
         <input type="number" onChange={ this.handleValueXChange } value={ this.props.x } />
-        <span>+</span>
+        <span>&times;</span>
         <input type="number" onChange={ this.handleValueYChange } value={ this.props.y } />
         <span>=</span>
         <input type="number" readOnly value={ this.props.x + this.props.y } />
       </div>
     );
   }
+}
+
+Summation.propTypes = {
+  onXChange: PropTypes.func,
+  onYChange: PropTypes.func,
+  x        : PropTypes.number,
+  y        : PropTypes.number
+}
+
+Summation.defaultProps = {
+  onXChange: () => 0,
+  onYChange: () => 0,
+  x        : 0,
+  y        : 0
 }
